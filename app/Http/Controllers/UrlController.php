e355979d434a1814cec853dd49675a937ae520be
salman_rahman <salmanrahman.cse@gmail.com>
2023-01-19 10:38:51 +0600

    basic api

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Url;
+use Facade\FlareClient\Http\Response;
+use Illuminate\Http\Request;
+
+
+class UrlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Url::latest()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $url = new Url;
+        // $url->original_url = $request->original_url;
+        // $url->shorten_url = $request->shorten_url;
+        // $url->save();
+
+        Url::create($request->all());
+        return response('created', 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Models/Url.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Url extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['original_url', 'shorten_url'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($url) {
+            $url->shorten_url = Str::random();
+        });
+    }
+}
